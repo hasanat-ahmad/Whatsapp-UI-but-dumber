87f6d933399480e59e5b20f6869f2b188c5381ba
hasanat-ahmad <sahibzadahasanatahmad@gmail.com>
2026-02-19 22:56:29 +0500

    feat: chats, status, calls added

--- lib/chats.dart
+++ lib/chats.dart
@@ -1,13 +1,94 @@
 import 'package:flutter/material.dart';
 
-class Chats extends StatelessWidget {
+class Chats extends StatefulWidget {
   const Chats({super.key});
 
   @override
+  State<Chats> createState() => _ChatsState();
+}
+
+class _ChatsState extends State<Chats> {
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Home Screen"),
+    return DefaultTabController(
+      length: 4,
+      child: Scaffold(
+        appBar: AppBar(
+          foregroundColor: Colors.white,
+          title: Text("WhatsApp"),
+          centerTitle: false,
+          backgroundColor: Color(0xff0ba142),
+          actions: [
+            Icon(Icons.search),
+            SizedBox(width: 10),
+            PopupMenuButton(
+              icon: Icon(Icons.more_vert_outlined),
+              itemBuilder: (context) => [
+                PopupMenuItem(value: 1, child: Text("New Group")),
+                PopupMenuItem(value: 2, child: Text("Setting")),
+                PopupMenuItem(value: 3, child: Text("Log Out")),
+              ],
+            ),
+            SizedBox(width: 10),
+          ],
+          bottom: TabBar(
+            tabs: [
+              Tab(child: Icon(Icons.camera_alt)),
+              Tab(child: Text("Chats")),
+              Tab(child: Text("Status")),
+              Tab(child: Text("Calls")),
+            ],
+          ),
+        ),
+        body: TabBarView(
+          children: [
+            Text("Camera"),
+            ListView.builder(
+              itemCount: 100,
+              itemBuilder: (context, index) => ListTile(
+                leading: CircleAvatar(
+                  backgroundImage: NetworkImage(
+                    'https://media.licdn.com/dms/image/v2/D4D03AQGEwuXxFV_IIQ/profile-displayphoto-crop_800_800/B4DZkl1i6dIcAI-/0/1757276420506?e=1773273600&v=beta&t=8n-Y_arem4mH_bi4dgbX-lV9KdGM55iJ_ZSEr1GYtEE',
+                  ),
+                ),
+                title: Text("Hasanat Ahmad"),
+                subtitle: Text("Snga ye khirana?"),
+                trailing: Text('6:56 pm'),
+              ),
+            ),
+
+            ListTile(
+              leading: CircleAvatar(
+                backgroundImage: NetworkImage(
+                  'https://media.licdn.com/dms/image/v2/D4D03AQGEwuXxFV_IIQ/profile-displayphoto-crop_800_800/B4DZkl1i6dIcAI-/0/1757276420506?e=1773273600&v=beta&t=8n-Y_arem4mH_bi4dgbX-lV9KdGM55iJ_ZSEr1GYtEE',
+                ),
+              ),
+              title: Text(
+                "My Status",
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+              subtitle: Text("Add to my status"),
+              trailing: CircleAvatar(child: Icon(Icons.camera_alt)),
+            ),
+            ListView.builder(
+              itemCount: 100,
+              itemBuilder: (context, index) => ListTile(
+                leading: CircleAvatar(
+                  backgroundImage: NetworkImage(
+                    'https://media.licdn.com/dms/image/v2/D4D03AQGEwuXxFV_IIQ/profile-displayphoto-crop_800_800/B4DZkl1i6dIcAI-/0/1757276420506?e=1773273600&v=beta&t=8n-Y_arem4mH_bi4dgbX-lV9KdGM55iJ_ZSEr1GYtEE',
+                  ),
+                ),
+                title: Text("Hasanat Ahmad"),
+                subtitle: Text(
+                  index % 2 == 0
+                      ? "You missed this audio call"
+                      : "You missed this video call",
+                ),
+                trailing: Icon(index % 2 == 0 ? Icons.phone : Icons.video_call),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
